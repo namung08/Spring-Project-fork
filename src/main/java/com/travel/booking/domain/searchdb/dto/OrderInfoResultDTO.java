6d9641333d89f6f027694aa0ddb26aac0c5a3425
namung08 <namung08@gmail.com>
2024-06-03 16:37:03 +0900

    fix: 결제 완료 확인을 로직 수정

--- src/main/java/com/travel/booking/domain/searchdb/dto/OrderInfoResultDTO.java
+++ src/main/java/com/travel/booking/domain/searchdb/dto/OrderInfoResultDTO.java
@@ -24,40 +24,40 @@ public class OrderInfoResultDTO {
     private LocalDate orderDate;
 
     public static OrderInfoResultDTO fromOrderAndSchedule(Order order, Schedule schedule, Stationinfo startStation, Stationinfo endStation) {
-        if (startStation.getStationType().getId() == 1) {
-            return OrderInfoResultDTO.builder()
-                    .startName(startStation.getStationName())
-                    .endName(endStation.getStationName())
-                    .orderDate(order.getOrderDate())
-                    .departureTime(schedule.getDepartureTime())
-                    .arrivalTime(schedule.getArrivalTime())
-                    .grade(schedule.getCarrier())
-                    .operatorName("고속 버스")
-                    .railName(startStation.getStationName() + " -> " + endStation.getStationName())
-                    .build();
-        } else if (startStation.getStationType().getId() == 2) {
-            return OrderInfoResultDTO.builder()
-                    .startName(startStation.getStationName())
-                    .endName(endStation.getStationName())
-                    .orderDate(order.getOrderDate())
-                    .departureTime(schedule.getDepartureTime())
-                    .arrivalTime(schedule.getArrivalTime())
-                    .grade(order.getGrade())
-                    .operatorName(schedule.getCarrier().split(",")[0])
-                    .railName(schedule.getFrequency())
-                    .build();
-        } else if (startStation.getStationType().getId() == 3) {
-            return OrderInfoResultDTO.builder()
-                    .startName(startStation.getStationName())
-                    .endName(endStation.getStationName())
-                    .orderDate(order.getOrderDate())
-                    .departureTime(schedule.getDepartureTime())
-                    .arrivalTime(schedule.getArrivalTime())
-                    .grade(order.getGrade())
-                    .operatorName(schedule.getCarrier().split(",")[0])
-                    .railName(schedule.getFrequency() + "번 열차")
-                    .build();
-        }
+            if (startStation.getStationType().getId() == 1) {
+                return OrderInfoResultDTO.builder()
+                        .startName(startStation.getStationName())
+                        .endName(endStation.getStationName())
+                        .orderDate(order.getOrderDate())
+                        .departureTime(schedule.getDepartureTime())
+                        .arrivalTime(schedule.getArrivalTime())
+                        .grade(schedule.getCarrier())
+                        .operatorName("고속 버스")
+                        .railName(startStation.getStationName() + " -> " + endStation.getStationName())
+                        .build();
+            } else if (startStation.getStationType().getId() == 2) {
+                return OrderInfoResultDTO.builder()
+                        .startName(startStation.getStationName())
+                        .endName(endStation.getStationName())
+                        .orderDate(order.getOrderDate())
+                        .departureTime(schedule.getDepartureTime())
+                        .arrivalTime(schedule.getArrivalTime())
+                        .grade(order.getGrade())
+                        .operatorName(schedule.getCarrier().split(",")[0])
+                        .railName(schedule.getFrequency())
+                        .build();
+            } else if (startStation.getStationType().getId() == 3) {
+                return OrderInfoResultDTO.builder()
+                        .startName(startStation.getStationName())
+                        .endName(endStation.getStationName())
+                        .orderDate(order.getOrderDate())
+                        .departureTime(schedule.getDepartureTime())
+                        .arrivalTime(schedule.getArrivalTime())
+                        .grade(order.getGrade())
+                        .operatorName(schedule.getCarrier().split(",")[0])
+                        .railName(schedule.getFrequency() + "번 열차")
+                        .build();
+            }
         return null;
     }
 }
